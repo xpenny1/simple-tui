153474f79fc89e3489c92529e8bef40773be55a6
Can Ward <can.ward.99@gmail.com>
2025-06-28 22:42:58 +0200

    fail

--- st.sh
+++ st.sh
@@ -2,16 +2,38 @@
 
 source ./ansi-lib/ansi-lib.sh
 
+trap "lines=$LINES;cols=$COLUMNS;" SIGWINCH
+
+
+centered1() {
+    currentline=$(echo $1)
+    maxlines=$(echo $2)
+    strcols=0
+    strlines=0
+    while IFS='' read line; do
+        strlines=$(( strlines + 1 ))
+        [[ ${#line} > $strcols ]] && strcols=${#line}
+    done <<< "$3"
+    echo $LINES
+    echo $COLUMNS
+    while IFS='' read line; do
+        printf "\e[""$(( ((lines - maxlines) / 2) + currentline ))"";""$(( (cols - strcols) / 2))""$CUP""%s" "$line"  >&2;
+    done <<< "$3"
+    echo $strlines
+}
 
 centered() {
     str=$(echo -e "$@")
-    strlines=$(printf "$str" | awk 'END{print NR}')
-    strcols=$(printf "$str" | awk 'length($0)>len{len=length($0)}END{print len}')
-    printf "$str" | awk '{print length($0),$0}END{print len}'
-    printf "$str"
-    echo $strlines
-    echo $strcols
-    printf "\e[""$(( (LINES - strlines) / 2 ))"";""$(( (COLUMNS - strcols) / 2))""$CUP""%s" "$str"
+    strlines=0
+    while IFS='' read -r line; do
+        strlines=$(( strlines + 1 ))
+    done <<< "$(printf \"$str\")"
+    currentline=0
+    while [[ -n "$1" ]]; do
+        printed=$(centered1 $currentline $strlines "$1")
+        currentline=$(( currentline + printed ))
+        shift
+    done
 }
 
 printCol() {
@@ -22,8 +44,14 @@ printCol() {
     printf "\e[38;5;""$fg""m\e[48;5;""$bg""m%s\e[0m\n" "$str"
 }
 
-centered $(printCol 212 - "Hallo\nWelt")
+printf "\e[?1049h"
+echo $LINES
+echo $COLUMNS
+centered1 0 1 "Hallo"
+centered "HAllo"
+##centered $(printCol 212 - "Hallo\nWelt")
 read a
+printf "\e[?1049l"
 
 #printf "\e[?1049h"
 #for i in $(seq 1 100); do
